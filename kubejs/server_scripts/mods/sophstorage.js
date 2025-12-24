fb3e613305248b165708e58e0bb6dae9803e96cc
Jeroen Wienk <jeroenwienk@gmail.com>
2025-12-24 16:42:21 +0100

    add own mods

--- kubejs/server_scripts/mods/sophstorage.js
+++ kubejs/server_scripts/mods/sophstorage.js
@@ -9,8 +9,8 @@ ServerEvents.recipes(event => {
     event.remove({ id: "sophisticatedbackpacks:xp_pump_upgrade" })
 
     // Remove Limited barrels
-    event.remove({ id: /^sophisticatedstorage:.*limited.+barrel.+$/ })
-    event.remove({ output: /^sophisticatedstorage:limited_barrel.+$/ })
+    // event.remove({ id: /^sophisticatedstorage:.*limited.+barrel.+$/ })
+    // event.remove({ output: /^sophisticatedstorage:limited_barrel.+$/ })
 
     // Remove Copper tier storage (not used)
     event.remove({ output: "sophisticatedstorage:copper_barrel" })
